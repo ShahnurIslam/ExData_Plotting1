6b6c10178f19f47cf7888bb6eda298321631f1b9
Shahnur Islam <shahnurislam@hotmail.co.uk>
2018-01-23 20:30:22 +0000

    Add files via upload

---
+++ plot2.R
@@ -0,0 +1,9 @@
+#Your working directory needs to contain the file 'household_power_consumption.txt' to work
+data <- read.csv("household_power_consumption.txt",sep = ";", stringsAsFactors = FALSE, na.strings = "?")
+data$datetime <- strptime(paste(data$Date,data$Time), "%d/%m/%Y %H:%M:%S")
+data$Date <- as.POSIXct(strptime(data$Date, "%d/%m/%Y"))
+data <- subset(data,(data$Date >= (as.POSIXct(as.Date("2007-02-01"))))
+               & (data$Date <= (as.POSIXct(as.Date("2007-02-02")))))
+with(data,plot(datetime,Global_active_power, type ="l", ylab = "Global Active Power(kilowatts)"))
+dev.copy(png,"plot2.png")
+dev.off()  
--- plot3.R
+++ plot3.R
@@ -4,14 +4,12 @@ data$datetime <- strptime(paste(data$Date,data$Time), "%d/%m/%Y %H:%M:%S")
 data$Date <- as.POSIXct(strptime(data$Date, "%d/%m/%Y"))
 data <- subset(data,(data$Date >= (as.POSIXct(as.Date("2007-02-01"))))
                & (data$Date <= (as.POSIXct(as.Date("2007-02-02")))))
-png(file = "plot4,png", width = 480, height = 480, units = "px")
-par(mfrow = c(2,2))
-with(data,plot(datetime,Global_active_power, type ="l", ylab = "Global Active Power", xlab = ""))
-with(data,plot(datetime,Voltage, type ="l", ylab = "Voltage"))
+
+
 with(data,plot(datetime,Sub_metering_1, type ="l",xlab = "", ylab = "Energy Sub Metering"))
 lines(data$datetime,data$Sub_metering_2,col="red")
 lines(data$datetime,data$Sub_metering_3,col="blue")
-legend("topright",bty="n", col= c("black", "red", "blue"), lty="solid",
-       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-with(data,plot(datetime,Global_reactive_power, type ="l", ylab = "Global_reactive_power"))
+legend("topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
+       lty=1,lwd =2,col=c("black","red","blue"))
+dev.copy(png,"plot3.png")
 dev.off()
